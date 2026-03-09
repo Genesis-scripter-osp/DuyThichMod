72b37ef9cc9e8eb814e693f730fbcbec44b8beeb
Genesis-scripter-osp <zatacutcanh2011@gmail.com>
2026-03-09 15:05:31 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,17 @@
 repeat task.wait() until game:IsLoaded()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Genesis-scripter-osp/DuyThichMod/main/main.lua"))()
+if _G.PhantomHub_Loader then
+    warn("PhantomHub loader đã chạy.")
+    return
+end
+_G.PhantomHub_Loader = true
+
+local URL = "https://raw.githubusercontent.com/Genesis-scripter-osp/DuyThichMod/main/main.lua"
+
+local success,err = pcall(function()
+    loadstring(game:HttpGet(URL))()
+end)
+
+if not success then
+    warn("Loader lỗi:",err)
+end
